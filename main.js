cac71ee52165e4d76c81f4c0f669c9b0f564a740
Niyarkalita2021 <82107686+Niyarkalita2021@users.noreply.github.com>
2021-08-03 20:43:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,5 +18,25 @@ function take_snapshot()
         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
+console.log('ml5 version:' , ml5.version);
 
-  console.log('ml5 version:' , ml5.version);
+classifier = ml5.imageClassifier('MobileNet' ,modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
